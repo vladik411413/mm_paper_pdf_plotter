3511297a1091bdbcff7e762be35f19ddaf6ece2f
Aleksandrov V.A. <vlad411413@yandex.com>
2025-11-09 16:09:50 +0300

    very good

--- plotpdf.py
+++ plotpdf.py
@@ -21,32 +21,23 @@ class Point(Pair):  # Inherit from Pair
         self.type = stype
     
     def mm(self, origin: Pair, tr: Transform):
-        """Convert to units"""
+        """Convert to units (Origin in MM)"""
 
         if self.type == SType.MM:
             return self
         # Else convert self from untis to mm
-        elif origin.type == SType.MM:
+        else:
             x_mm = origin.x + (self.x) / tr.ox.scale
             y_mm = origin.y + (self.y) / tr.oy.scale
             return Point(x_mm, y_mm, SType.MM)
-        elif origin.type == SType.UNITS:
-            x_mm = (- origin.x + (self.x)) / tr.ox.scale
-            y_mm = (- origin.y + (self.y)) / tr.oy.scale
-            return Point(x_mm, y_mm, SType.MM)
     
     
-    def units(self, origin: Point, tr: Transform):
+    def units(self, origin: Pair, tr: Transform):
         """Convert to units"""
 
         if self.type == SType.UNITS:
             return self
-        # Else convert self from mm to units
-        elif origin.type == SType.UNITS:
-            x_units = origin.x + (self.x) * tr.ox.scale
-            y_units = origin.y + (self.y) * tr.oy.scale
-            return Point(x_units, y_units, SType.UNITS)
-        elif origin.type == SType.MM:
+        else:
             x_units = (- origin.x + (self.x)) * tr.ox.scale
             y_units = (- origin.y + (self.y)) * tr.oy.scale
             return Point(x_units, y_units, SType.UNITS)
@@ -56,11 +47,11 @@ class Point(Pair):  # Inherit from Pair
 
 class Paper:
     def __init__(self,width,height, dw, dh, pdf_filename):
-        self.w = width
-        self.h = height
         self.c = canvas.Canvas(pdf_filename, pagesize=(width, height))
-        self.dw = dw
-        self.dh = dh
+        self.w = width / mm
+        self.h = height / mm
+        self.dw = dw / mm
+        self.dh = dh / mm
     @property
     def center(self):
         return Point((self.w) / 2, (self.h) / 2, SType.MM)
@@ -68,7 +59,7 @@ class Paper:
     def left(self):
         return (self.w - self.dw) / 2
     @property
-    def down(self):
+    def bottom(self):
         return (self.h - self.dh) / 2
     @property
     def right(self):
@@ -92,7 +83,7 @@ class Plot:
             self.y = df["y"]
             self.xmin, self.xmax = df["x"].min(), df["x"].max()
             self.ymin, self.ymax = df["y"].min(), df["y"].max()
-            print(f"✅ Read {len(points)} points from {csv_filename}")
+            print(f"✅ Read {len(self.points)} points from {csv_filename}")
             print(f"   X range: {xmin:.2f} → {xmax:.2f}")
             print(f"   Y range: {ymin:.2f} → {ymax:.2f}")
 
@@ -101,44 +92,21 @@ class Plot:
 
     @property
     def range(self):
-        return Point(xmax-xmin,ymax-ymin,SType.UNITS)
+        return Point(self.xmax-self.xmin,self.ymax-self.ymin,SType.UNITS)
 
     
 
 
-def draw_scaled_grid(page: Paper, plot: Plot, tr: Transform):
+def draw_scaled_grid(page: Paper, origin: Pair,  plot: Plot, tr: Transform):
     """Draw millimeter grid + scaled axes and labels"""
     # === Millimeter grid ===
+    '''
     page.c.saveState()
-    for x in range(-int(page.dw)/2,int(page.dw)/2 + 1):
-        x_pos = page.center.x + x
-        if x % 10 == 0:
-            page.c.setStrokeColorRGB(0.6, 0.6, 0.6)
-            page.c.setLineWidth(0.6)
-        else:
-            page.c.setStrokeColorRGB(0.85, 0.85, 0.85)
-            page.c.setLineWidth(0.25)
-        page.c.line(x_pos*mm, page.bottom*mm, x_pos*mm, page.top * mm)
 
-    for y in range(-int(page.dh)/2,int(page.dh)/2 + 1):
-        y_pos = page.center.y + y
-        if y % 10 == 0:
-            page.c.setStrokeColorRGB(0.6, 0.6, 0.6)
-            page.c.setLineWidth(0.6)
-        else:
-            page.c.setStrokeColorRGB(0.85, 0.85, 0.85)
-            page.c.setLineWidth(0.25)
-        page.c.line(page.left*mm, y_pos*mm, page.right*mm, y_pos*mm)
+
+
     page.c.restoreState()
-
-    # === Axes and ticks ===
-    origin = Point(page.left, page.bottom, SType.MM)
-
-    # Axes lines
-    page.c.setStrokeColorRGB(0, 0, 0)
-    page.c.setLineWidth(1.8)
-    page.c.line(origin.x*mm, origin.y*mm, origin.x*mm, page.top*mm)
-    page.c.line(origin.x*mm, origin.y*mm, page.right*mm, origin.y*mm)
+    '''
 
     # Tick step [units]
     tick_step_x = tr.ox.power / 10
@@ -150,70 +118,82 @@ def draw_scaled_grid(page: Paper, plot: Plot, tr: Transform):
     # X ticks and labels
     tx0 = Point(plot.xmin - (plot.xmin % tick_step_x),0,SType.UNITS)
     tx=tx0
-    while tx.mm(origin,tr).x <= plot.xmax:
-        page.c.line(
-            tx.mm(origin,tr).x, 
-            origin.y - 2 * mm, 
-            tx.mm(origin,tr).x, 
-            origin.y + 2 * mm)
-        page.c.drawString(
-            tx.mm(origin,tr).x - 2,
-            origin.y - 6 * mm, 
-            f"{tx:g}")
-        tx.x += tick_step_x
 
-    # X ticks and labels
-    tx=tx0
-    while tx.mm(origin,tr).x >= plot.xmin:
-        page.c.line(
-            tx.mm(origin,tr).x, 
-            origin.y - 2 * mm, 
-            tx.mm(origin,tr).x, 
-            origin.y + 2 * mm)
-        page.c.drawString(
-            tx.mm(origin,tr).x - 2,
-            origin.y - 6 * mm, 
-            f"{tx:g}")
+    while tx.mm(origin,tr).x > page.left + 10:
         tx.x -= tick_step_x
+    corrx = tx.mm(origin,tr).x 
+    for x in range(-int(page.w),int(page.w)):
+        x_pos = corrx + x
+        if x % 10 == 0:
+            page.c.setStrokeColorRGB(0.6, 0.6, 0.6)
+            page.c.setLineWidth(0.6)
+        else:
+            page.c.setStrokeColorRGB(0.85, 0.85, 0.85)
+            page.c.setLineWidth(0.25)
+        page.c.line(x_pos*mm, 0*mm, x_pos*mm, page.h*mm)
+
 
     # Y ticks and labels
     ty0 = Point(plot.ymin - (plot.ymin % tick_step_y),0,SType.UNITS)
     ty=ty0
-    while ty.mm(origin,tr).y <= plot.ymay:
-        page.c.line(
-            ty.mm(origin,tr).y, 
-            origin.y - 2 * mm, 
-            ty.mm(origin,tr).y, 
-            origin.y + 2 * mm)
-        page.c.drawString(
-            ty.mm(origin,tr).y - 2,
-            origin.y - 6 * mm, 
-            f"{ty:g}")
-        ty.y += tick_step_y
-
-    
-    # X ticks and labels
-    ty=ty0
-    while ty.mm(origin,tr).y >= plot.ymin:
-        page.c.line(
-            ty.mm(origin,tr).y, 
-            origin.y - 2 * mm, 
-            ty.mm(origin,tr).y, 
-            origin.y + 2 * mm)
-        page.c.drawString(
-            ty.mm(origin,tr).y - 2,
-            origin.y - 6 * mm, 
-            f"{ty:g}")
+    while ty.mm(origin,tr).y > page.bottom + 10:
         ty.y -= tick_step_y
+    corry = tx.mm(origin,tr).y
+    for y in range(-int(page.h),int(page.h)):
+        y_pos = corry + float(y)
+        if y % 10 == 0:
+            page.c.setStrokeColorRGB(0.6, 0.6, 0.6)
+            page.c.setLineWidth(0.6)
+        else:
+            page.c.setStrokeColorRGB(0.85, 0.85, 0.85)
+            page.c.setLineWidth(0.25)
+        page.c.line(0*mm, y_pos*mm, page.w*mm, y_pos*mm)
+
+    page.c.setLineWidth(0.8)
+    page.c.setStrokeColorRGB(0, 0, 0)
+
+    corry = corry % 10.0 + 10
+    while tx.mm(origin,tr).x < page.right - 10:
+        page.c.line(
+            tx.mm(origin,tr).x*mm, 
+            corry*mm  - 1*mm , 
+            tx.mm(origin,tr).x*mm , 
+            corry*mm  + 1*mm )
+        page.c.drawString(
+            tx.mm(origin,tr).x*mm  - 1*mm ,
+            corry*mm  - 6*mm , 
+            f"{tx.x}")
+        tx.x += tick_step_x
+
+    corrx = corrx % 10.0 + 5
+    while ty.mm(origin,tr).y < page.top - 10:
+        page.c.line(
+            corrx*mm  - 1*mm ,
+            ty.mm(origin,tr).y*mm , 
+            corrx*mm  + 1*mm ,
+            ty.mm(origin,tr).y*mm )
+            
+        page.c.drawString(
+            corrx*mm  - 6*mm , 
+            ty.mm(origin,tr).y*mm  - 1*mm ,
+            f"{ty.y:g}")
+        ty.y += tick_step_y
+    
 
 
-def plot_points(c, points, x_offset, y_offset, xmin, xmax, ymin, ymax, sx, sy):
+    # Axes lines
+    page.c.setStrokeColorRGB(0, 0, 0)
+    page.c.setLineWidth(0.8)
+    page.c.line(corrx*mm, corry*mm, corrx*mm, page.top*mm)
+    page.c.line(corrx*mm, corry*mm, page.right*mm, corry*mm)
+
+
+def plot_points(page, origin, plot, tr):
     """Plot data points"""
     page.c.setFillColorRGB(1, 0, 0)
-    for x, y in points:
-        px = x_offset + ((x - (xmin+xmax)/2) / sx) * mm
-        py = y_offset + ((y - (ymin+ymax)/2) / sy) * mm
-        page.c.circle(px, py, 1.5, fill=1, stroke=0)
+    for x, y in plot.points:
+        point = Point(x,y,SType.UNITS)
+        page.c.circle(point.mm(origin,tr).x*mm, point.mm(origin,tr).y*mm, 1.5, fill=1, stroke=0)
 
 
 def create_pdf_from_csv(csv_filename, pdf_filename="auto_scaled_plot.pdf"):
@@ -230,31 +210,21 @@ def create_pdf_from_csv(csv_filename, pdf_filename="auto_scaled_plot.pdf"):
     print("📏 Scale computed by Transform:")
     print(tr)
 
+    # Origin (mm)
+    origin = Pair(
+        (page.center.x- (plot.xmax+plot.xmin)/(2*tr.ox.scale)), 
+        (page.center.y- (plot.ymax+plot.ymin)/(2*tr.oy.scale)))
+
     # Draw background grid and axes
-    draw_scaled_grid(page, plot, tr)
-
-    '''
-    bottom_left_corner = Point((page.width - drawing.width) / 2,
-     (page.height - drawing.height) / 2)
-
-    # Draw border
-    page.c.setStrokeColorRGB(0, 0, 0)
-    page.c.setLineWidth(1)
-    page.c.rect(x_offset, y_offset, DRAWING_WIDTH_MM * mm, DRAWING_HEIGHT_MM * mm)
+    draw_scaled_grid(page, origin, plot, tr)
 
     # Plot data points
-    plot_points(c, points, x_offset, y_offset, xmin, xmax, ymin, ymax, sx, sy)
+    plot_points(page, origin, plot, tr)
 
-    # Info
-    page.c.setFont("Helvetica", 9)
-    page.c.setFillColorRGB(0, 0, 0)
-    page.c.drawString(x_offset, y_offset - 15, f"Points plotted: {len(points)}")
-    page.c.drawString(x_offset, y_offset - 30, f"Scale X: {sx:.4f} units/mm | Scale Y: {sy:.4f} units/mm")
-    '''
     page.c.save()
     print(f"✅ PDF created: {pdf_filename}")
 
-
+'''
 # === Sample test ===
 def create_sample_csv(filename="sample_points.csv"):
     sample = [
@@ -273,7 +243,7 @@ def create_sample_csv(filename="sample_points.csv"):
     df = pd.DataFrame(sample)
     df.to_csv(filename, index=False, header=False)
     print(f"Sample CSV created: {filename}")
-
+'''
 
 if __name__ == "__main__":
     #create_sample_csv("sample_points.csv")
